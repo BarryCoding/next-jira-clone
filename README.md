d9cb2d9804dfea6d8f8b9a1cdfbb01d67886f80a
Springer <zxllks@yahoo.com>
2024-10-13 17:24:51 +0800

    UI: shadcn components, customize Button, Input, Select

--- README.md
+++ README.md
@@ -36,6 +36,14 @@ bunx --bun shadcn@2.1.0 init
 bunx --bun shadcn@2.1.0 add button
 ```
 
+### tailwind
+
 - vscode extensions for tailwind `bradlc.vscode-tailwindcss`
 
-- tailwind config
+- tailwind config modify content to add `features`
+
+```bash
+bunx --bun shadcn@2.1.0 add
+
+# avatar badge calendar card chart checkbox dialog drawer dropdown-menu form input label popover scroll-area select separator sheet skeleton sonner table tabs textarea
+```
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 export default function Home() {
   return (
     <div>
-      <Button size='sm'>Click me</Button>
-      <p className=' text-red-500 font-semibold'>Springer</p>
+      <Input />
+      <Button>Primary</Button>
+
+      <Button variant='secondary'>Secondary</Button>
+      <Button variant='destructive'>destructive</Button>
+      <Button variant='outline'>outline</Button>
+      <Button variant='ghost'>ghost</Button>
+      <Button variant='muted'>muted</Button>
+      <Button variant='tertiary'>tertiary</Button>
     </div>
   )
 }
